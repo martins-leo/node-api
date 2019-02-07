3543f369b5bbcf60a65a4bb200769ee23addf27f
Leonardo Martins <leonardo.martins@mytapp.com.br>
2019-02-07 10:01:41 -0200

    adicionando rota get para produtos

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -3,19 +3,25 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
-exports.post = ('/', (req, res, next) => {
+exports.get = (req, res, next) => {
+    Product.find({active: true}, 'title price slug')
+    .then(data => {
+        res.status(200).send(data);
+    }).catch(e => {
+        res.status(400).send(e);
+    });
+}
+
+exports.post = (req, res, next) => {
     var product = new Product(req.body);
-    product.save().then(x => {
-        res.status(201).send({ message: 'Produto cadastrado com sucesso' })
-        .catch(e => {
-            res.status(400).send({
-                message: 'Falha ao cadastrar produto',
-                data: res.body
-            })
-        })
-    })
-    res.status(201).send(req.body);
-});
+    product
+        .save()
+        .then(x => {
+            res.status(201).send({ message: 'Produto cadastrado com sucesso' });
+        }).catch(e => {
+            res.status(400).send({message: 'Falha ao cadastrar produto', data: e});
+        });
+};
 
 exports.put = ('/:id', (req, res, next) => {
     const id = req.params.id;
